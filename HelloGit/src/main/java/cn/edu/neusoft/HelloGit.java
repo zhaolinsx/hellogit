de0494b3e689373fa2bd828a4c598e47d3ddcf2c
zhaolin_sx <zhaolin_sx@neusoft.edu.cn>
2020-07-27 09:54:34 +0800

    分支添加

--- HelloGit/src/main/java/cn/edu/neusoft/HelloGit.java
+++ HelloGit/src/main/java/cn/edu/neusoft/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
         System.out.println("分支添加");
         System.out.println("在线编辑");
         System.out.println("刚刚在线编辑");
+        System.out.println("正在编辑");
     }
 }
